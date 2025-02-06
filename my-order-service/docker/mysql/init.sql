958560d2baa5249dcd7894326716e194f7156d86
Shoaib167 <shoaibh@touchpointstechnology.com>
2025-02-06 15:47:02 +0530

    fix(order service) : mysql data folder of docker added into gitignore

---
+++ my-order-service/docker/mysql/init.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE NULLIF NOT EXISTS order_service;
---
+++ my-order-service/src/main/java/com/sh_order/microservice/order/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.sh_order.microservice.order.service;
+
+import com.sh_order.microservice.order.dto.OrderRequest;
+import com.sh_order.microservice.order.model.Order;
+import com.sh_order.microservice.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        var order = mapToOrder(orderRequest);
+        orderRepository.save(order);
+    }
+
+    private static Order mapToOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setQuantity(orderRequest.quantity());
+        order.setSkuCode(orderRequest.skuCode());
+        return order;
+    }
+}
