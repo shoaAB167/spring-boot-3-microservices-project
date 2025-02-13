d521764f93476af5f2a578c7526451ec9776bdc9
Shoaib167 <shoaibh@touchpointstechnology.com>
2025-02-14 01:08:40 +0530

    feat(inventory service) : basic API added for inventory service with docker file container for mysql database

---
+++ my-inventory-service/src/test/java/com/sh_inventory/microservice/inventoryservice/TestInventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package com.sh_inventory.microservice.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(InventoryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
