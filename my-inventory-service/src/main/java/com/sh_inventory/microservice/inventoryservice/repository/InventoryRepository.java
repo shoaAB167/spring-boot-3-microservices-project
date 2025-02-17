bbec265b87f95c1edcae20338c6f40e4f74c0060
Shoaib167 <shoaibh@touchpointstechnology.com>
2025-02-18 00:13:36 +0530

    feat(order service) :added synchronous communication using openfeign between order and inventory service with integration tests

---
+++ my-inventory-service/src/main/java/com/sh_inventory/microservice/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.sh_inventory.microservice.inventoryservice.repository;
+
+import com.sh_inventory.microservice.inventoryservice.model.Inventory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, int quantity);
+}
