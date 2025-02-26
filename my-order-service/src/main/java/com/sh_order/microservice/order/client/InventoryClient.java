f3a367dc366d4bf8e91b7322dc5ab3495035ac29
Shoaib167 <shoaibh@touchpointstechnology.com>
2025-02-26 11:33:14 +0530

    implemented circuit breaker for api gateway and for order to inventory service

--- my-order-service/src/main/java/com/sh_order/microservice/order/client/InventoryClient.java
+++ my-order-service/src/main/java/com/sh_order/microservice/order/client/InventoryClient.java
@@ -1,10 +1,25 @@
 package com.sh_order.microservice.order.client;
 
+import groovy.util.logging.Slf4j;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@Slf4j
 public interface InventoryClient {
 
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory") //used during addition of application properties
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skucode {}, failure reason: {}", code, throwable.getMessage());
+        return false;
+    }
 }
