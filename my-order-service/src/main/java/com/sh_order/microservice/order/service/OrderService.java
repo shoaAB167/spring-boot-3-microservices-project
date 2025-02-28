0c3588f45bf3892828b6f309efbcdc0993c446c3
Shoaib167 <shoaibh@touchpointstechnology.com>
2025-02-28 17:42:43 +0530

    kafka integration(order-notification) : integration of kafka and modified code for async communication between order and notification service with schema registry for kafka topic

--- my-order-service/src/main/java/com/sh_order/microservice/order/service/OrderService.java
+++ my-order-service/src/main/java/com/sh_order/microservice/order/service/OrderService.java
@@ -2,28 +2,49 @@ package com.sh_order.microservice.order.service;
 
 import com.sh_order.microservice.order.client.InventoryClient;
 import com.sh_order.microservice.order.dto.OrderRequest;
+import com.sh_order.microservice.order.event.OrderPlacedEvent;
 import com.sh_order.microservice.order.model.Order;
 import com.sh_order.microservice.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import groovy.util.logging.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         //calling inventory service here to check stock availability
         boolean inStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
         if(inStock){
             var order = mapToOrder(orderRequest);
             orderRepository.save(order);
+
+            //sent to kafka topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
+
+            log.info("Start- Sending OrderPlacedEvent {} to Kafka Topic", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End- Sending OrderPlacedEvent {} to Kafka Topic", orderPlacedEvent);
         }else{
             throw new RuntimeException("Product with skucode " + orderRequest.skuCode() + "is not in stock");
         }
@@ -32,7 +53,7 @@ public class OrderService {
     private static Order mapToOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
+        order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
         order.setQuantity(orderRequest.quantity());
         order.setSkuCode(orderRequest.skuCode());
         return order;
