97d75ee84299b019ee5f0da154da7c0c54c96fda
Shoaib167 <shoaibh@touchpointstechnology.com>
2025-03-06 18:03:39 +0530

    feature(observalibility):implemented observalibility using grafana stack, added required docker images and made needed modification

--- api-gateway/src/main/java/com/sh/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/sh/api_gateway/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
-    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/aggregate/**"};
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/aggregate/**", "*/actuator/prometheus"};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
